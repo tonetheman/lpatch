6529de1a5a82103696b5f8753c914008bf0a1909
tonethemna <tonetheman@gmail.com>
2022-08-13 17:35:27 -0500

    adding readme and patcher.lua example

---
+++ patcher.lua
@@ -0,0 +1,39 @@
+
+--[[
+    this is an example patcher.lua file
+
+    this is a big ass comment by the way in lua
+
+    lua 5.4 in the house
+]]
+
+-- yup you can even define functions
+function dbg(...)
+    io.write("dbg---:")
+    for i,v in ipairs({...}) do
+        print(i,v)
+    end
+end
+
+-- patch_getbyte will return a byte value from the input file buffer
+local val = patch_getbyte(200)
+dbg("val inside of lua",val)
+
+-- patch_getfilesize will return the filesize of the input buffer
+local filesize = patch_getfilesize()
+dbg("filesize we are patching",filesize)
+
+-- patch_hello - just making sure things work
+dbg("check we can talk to c")
+patch_hello()
+
+dbg("----------------")
+-- patch_setbyte(index,value) = will set the value of the byte at position index
+-- this will only change the buffer in memory
+patch_setbyte(0,2)
+
+-- patch_writefile will write the file back to disk, the original input file is
+-- left untouched
+patch_writefile("testing")
+dbg("patcher.lua")
+
---
+++ readme.md
@@ -0,0 +1,18 @@
+
+# lpatch
+
+a weird patcher of files...
+
+to run
+```
+lpatch --input|-i filename
+```
+
+to build
+```
+make
+```
+
+will create lpatch in current directory.
+
+it will read the input file you tell it on the command line and then read the file patcher.lua from the disk and run it
